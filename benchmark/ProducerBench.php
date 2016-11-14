1ce3cf306d160c06bbc985ba5efc0d5369247eda
Alex Vasilenko <oleksandr.vasylenko@meinfernbus.de>
2016-11-14 21:32:29 +0100

    add amqp-ext to benchmarks
    include queue declaration in cleanup to reduce rabbitmq creation overhead

--- benchmark/ProducerBench.php
+++ benchmark/ProducerBench.php
@@ -6,7 +6,6 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 use PhpBench\Benchmark\Metadata\Annotations\BeforeMethods;
 use PhpBench\Benchmark\Metadata\Annotations\ParamProviders;
-use PhpBench\Benchmark\Metadata\Annotations\Revs;
 
 /**
  * @BeforeMethods({"cleanup"})
@@ -17,14 +16,14 @@ class ProducerBench
 
     private $queueName = 'bench_queue';
 
-    public function connectionClass()
+    public function keepAlive()
     {
         return [
             [
-                'connection' => AMQPStreamConnection::class,
+                'alive' => true,
             ],
             [
-                'connection' => AMQPSocketConnection::class,
+                'alive' => false,
             ]
         ];
     }
@@ -47,12 +46,12 @@ class ProducerBench
     public function messageLength()
     {
         return [
-            [
-                'length' => 10,
-            ],
-            [
-                'length' => 1000,
-            ],
+//            [
+//                'length' => 10,
+//            ],
+//            [
+//                'length' => 1000,
+//            ],
             [
                 'length' => 10000,
             ]
@@ -68,19 +67,94 @@ class ProducerBench
         $ch->queue_delete($this->queueName);
         $ch->exchange_delete($this->exchangeName);
 
+        //redeclare to not benchmark rabbitmq creation of queue
+        $ch->queue_declare($this->queueName, false, true, false, false);
+        $ch->exchange_declare($this->exchangeName, 'direct', false, true, false);
+
+        $ch->queue_bind($this->queueName, $this->exchangeName);
+
         $ch->close();
         $conn->close();
     }
 
     /**
-     * @ParamProviders({"connectionClass", "messagesCount", "messageLength"})
+     * @ParamProviders({"messageLength", "keepAlive", "messagesCount"})
      *
      * @param array $params
      */
-    public function benchPublish($params)
+    public function benchStreamConnection($params)
     {
         /** @var AbstractConnection $conn */
-        $conn = new $params['connection'](HOST, PORT, USER, PASS, VHOST, false, 'AMQPLAIN', null, 'en_US', 3, 3, null, true);
+        $conn = new AMQPStreamConnection(HOST, PORT, USER, PASS, VHOST, false, 'AMQPLAIN', null, 'en_US', 3, 3, null, $params['alive']);
+        $this->produceWithLibConnection($conn, $params);
+    }
+
+    /**
+     * @ParamProviders({"messageLength", "keepAlive", "messagesCount"})
+     *
+     * @param array $params
+     */
+    public function benchSocketConnection($params)
+    {
+        /** @var AbstractConnection $conn */
+        $conn = new AMQPSocketConnection(HOST, PORT, USER, PASS, VHOST, false, 'AMQPLAIN', null, 'en_US', 3, $params['alive']);
+        $this->produceWithLibConnection($conn, $params);
+    }
+
+    /**
+     * @ParamProviders({"messageLength", "messagesCount"})
+     *
+     * @param array $params
+     */
+    public function benchExtension($params)
+    {
+        // Create a connection
+        $cnn = new \AMQPConnection();
+        $cnn->setHost(HOST);
+        $cnn->setPort(PORT);
+        $cnn->setLogin(USER);
+        $cnn->setPassword(PASS);
+        $cnn->setVhost(VHOST);
+        $cnn->connect();
+
+        // Create a channel
+        $ch = new \AMQPChannel($cnn);
+
+        // Declare a new exchange
+        $ex = new \AMQPExchange($ch);
+        $ex->setName($this->exchangeName);
+        $ex->setType("direct");
+        $ex->setFlags(AMQP_DURABLE);
+        $ex->declareExchange();
+
+        // Create a new queue
+        $q = new \AMQPQueue($ch);
+        $q->setName($this->queueName);
+        $q->setFlags(AMQP_DURABLE);
+        $q->declareQueue();
+
+        // Bind it on the exchange to routing.key
+        $q->bind($this->exchangeName);
+
+        $body = 'a';
+        for ($i = 0; $i < $params['length']; $i++) {
+            $body .= ord($i % 255);
+        }
+
+        for ($i = 0; $i < $params['messages']; $i++) {
+            $ex->publish($body, null, AMQP_DURABLE);
+        }
+
+        //Disconnect
+        $cnn->disconnect();
+    }
+
+    /**
+     * @param AbstractConnection $conn
+     * @param array              $params
+     */
+    private function produceWithLibConnection(AbstractConnection $conn, $params)
+    {
         $ch = $conn->channel();
 
         $ch->queue_declare($this->queueName, false, true, false, false);
